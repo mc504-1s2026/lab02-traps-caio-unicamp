241ae3e5436797b8a97ad7898fda98703f62e019
caio-unicamp <c288808@dac.unicamp.br>
2026-07-13 14:18:54 -0300

    Começa a implementação dos trap handles, apenas copiei o do kmain do ktest do lab 1 e quebrei em dois, falta implementar o resto

--- src/trap.c
+++ src/trap.c
@@ -1,8 +1,22 @@
 #include <kernel/trap.h>
 #include <kernel/panic.h>
+#include <include/arch/csr.h>
+
+
+#define TRAP_IRQ_BIT			(1ULL << 63)
+#define TRAP_CODE_MASK			(TRAP_IRQ_BIT - 1)
+#define TRAP_INST_ACCESS_FAULT		1
+#define TRAP_LOAD_ACCESS_FAULT		5
+#define TRAP_STORE_ACCESS_FAULT		7
+#define TRAP_INST_PAGE_FAULT		12
+#define TRAP_LOAD_PAGE_FAULT		13
+#define TRAP_STORE_PAGE_FAULT		15
 
 /* defined in src/trap_entry.S */
 extern void trap_entry();
+static bool ktest_fault_occurred = false;
+static bool ktest_fault_expected = false;
+static u64 ktest_fault_return_addr = 0;
 
 void handle_irq()
 {
@@ -12,20 +26,56 @@ void handle_irq()
 
 void handle_exception()
 {
-	/* not implemented */
-	BUG();
+	static u64 scause, stval, sepc;
+	
+
+	scause = csr_read(CSR_SCAUSE);
+	stval = csr_read(CSR_STVAL);
+	sepc = csr_read(CSR_SEPC);
+
+	u64 exception_code = scause & TRAP_CODE_MASK;	// Pegando apenas o código da exceção
+	
+	switch (exception_code) {
+		case TRAP_INST_ACCESS_FAULT:
+			error("instruction access fault at address 0x%x, sepc = 0x%x\n", stval, sepc);
+			break;
+		case TRAP_LOAD_ACCESS_FAULT:
+			error("load access fault at address 0x%x, sepc = 0x%x\n", stval, sepc);
+			break;
+		case TRAP_STORE_ACCESS_FAULT:
+			error("store access fault at address 0x%x, sepc = 0x%x\n", stval, sepc);
+			break;
+		case TRAP_INST_PAGE_FAULT:
+			error("instruction page fault at address 0x%x, sepc = 0x%x\n", stval, sepc);
+			break;
+		case TRAP_LOAD_PAGE_FAULT:
+			error("load page fault at address 0x%x, sepc = 0x%x\n", stval, sepc);
+			break;
+		case TRAP_STORE_PAGE_FAULT:
+			error("store page fault at address 0x%x, sepc = 0x%x\n", stval, sepc);
+			break;
+		default:
+			error("uncaught exception! cause: 0x%x, sepc = \n", scause, sepc);
+	}	
+
+	panic("unexpected fault!\n");
 }
 
 void trap_setup()
 {
-	/* not implemented */
-	BUG();
+    // Grava o endereço do handler de assembly no CSR stvec
+    csr_write(CSR_STVEC, trap_entry);
 }
 
 void handle_trap()
 {
-	/* not implemented */
-	BUG();
+	u64 scause = csr_read(CSR_SCAUSE);
+
+	if (scause & TRAP_IRQ_BIT){	// Analisa o bit 63 pra decidir ver se é uma irq ou uma exceção
+		handle_irq();
+	}else{
+		handle_exception();
+	}
 }
 
 void hart_irq_enable()
