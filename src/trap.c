a9f712a0b7bedc03f193cf557f61a629b93939b7
caio-unicamp <c288808@dac.unicamp.br>
2026-07-13 14:31:46 -0300

    Finaliza o trap.c com a chamada do timer que vai ser implementada

--- src/trap.c
+++ src/trap.c
@@ -1,7 +1,7 @@
 #include <kernel/trap.h>
 #include <kernel/panic.h>
 #include <include/arch/csr.h>
-
+#include <include/arch/plic.h>
 
 #define TRAP_IRQ_BIT			(1ULL << 63)
 #define TRAP_CODE_MASK			(TRAP_IRQ_BIT - 1)
@@ -12,23 +12,61 @@
 #define TRAP_LOAD_PAGE_FAULT		13
 #define TRAP_STORE_PAGE_FAULT		15
 
+// Códigos de interrupção (IRQ) do RISC-V em Modo Supervisor
+#define IRQ_S_TIMER             5
+#define IRQ_S_EXT               9
+
+// O IRQ da UART no QEMU
+#define UART_IRQ                10
+
+// Declarações das funções que iremos implementar nos outros arquivos
+extern void timer_irq();
+extern void serial_irq();
+
 /* defined in src/trap_entry.S */
 extern void trap_entry();
+
 static bool ktest_fault_occurred = false;
 static bool ktest_fault_expected = false;
 static u64 ktest_fault_return_addr = 0;
 
 void handle_irq()
 {
-	/* not implemented */
-	BUG();
+    u64 scause = csr_read(CSR_SCAUSE);
+    u64 irq_code = scause & TRAP_CODE_MASK; // Remove o bit 63 para pegar o código da irq
+
+    switch (irq_code) {
+        case IRQ_S_TIMER:
+            // Interrupção de timer
+            timer_irq();
+            break;
+
+        case IRQ_S_EXT: {
+            // Interrupção externa
+            int irq = plic_hart_claim_irq(0);	// Está sempre no hart 0
+            
+            if (irq != 0) {
+                if (irq == UART_IRQ) {
+                    serial_irq(); // Processa os dados da porta serial
+                } else {
+                    error("unexpected external irq: %d\n", irq);
+                }
+                // Avisa ao PLIC que terminou de processar essa IRQ
+                plic_hart_complete_irq(0, irq);
+            }
+            break;
+        }
+
+        default:
+            error("uncaught interrupt! cause: %llu\n", irq_code);
+            panic("unexpected interrupt!\n");
+    }
 }
 
 void handle_exception()
 {
 	static u64 scause, stval, sepc;
 	
-
 	scause = csr_read(CSR_SCAUSE);
 	stval = csr_read(CSR_STVAL);
 	sepc = csr_read(CSR_SEPC);
